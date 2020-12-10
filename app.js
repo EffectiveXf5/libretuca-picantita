9af18ce08414fbfa55598978cdcb299eb84c8cb7
Cristian2062 <baner2062@gmail.com>
2020-12-10 15:16:56 +0100

    Avance de la guía

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'hbs');
+//Sets handlebars configurations (we will go through them later on)
+app.engine('hbs', handlebars({
+layoutsDir: __dirname + '/views/layouts',
+extname: 'hbs',
+defaultLayout: 'planB',
+}));
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+res.render('main', {layout:'index'});
+});
+//app.get('/planB', (req, res) =>{
+    //res.render('main', {layout:'planB'});
+//});
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
+
+
+
+
