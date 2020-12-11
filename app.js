6123c23b31236460dc05dd28e385fef2db3df0f5
Cristian2062 <baner2062@gmail.com>
2020-12-11 11:03:45 +0100

    Cambios Edu

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 5500;
-//Loads the handlebars module
 const handlebars = require('express-handlebars');
-//Sets our app to use the handlebars engine
+
 app.set('view engine', 'hbs');
-//Sets handlebars configurations (we will go through them later on)
+
 app.engine('hbs', handlebars({
 layoutsDir: __dirname + '/views/layouts',
 extname: 'hbs',
-defaultLayout: 'planB',
-partialsDir: __dirname + '/views/partials/'
+partialsDir: __dirname + '/views/partials/',
 }));
-app.use(express.static('public'))
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
 res.render('main', {layout:'index'});
 });
-//app.get('/planB', (req, res) =>{
-    //res.render('main', {layout:'planB'});
-//});
+
 app.listen(port, () => console.log(`App listening to port ${port}`));
 
 
