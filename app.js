68dcd87c95c3ba739aa4bc38de1dbb9474ebc755
Arturo <arturomf88@gmail.com>
2020-12-21 14:06:17 +0100

    Detalle libro 2

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5500;
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
-app.engine('hbs', handlebars({
-layoutsDir: __dirname + '/views/layouts',
-extname: 'hbs',
-partialsDir: __dirname + '/views/partials/',
-}));
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-res.render('main', {layout:'index'});
+app.get("/", (req, res) => {
+  res.render("main", { layout: "index" });
 });
 
-app.get('/relatoseroticos', (req, res) => {
-res.render('templateLibro1', {layout:'planB'});
+app.get("/relatoseroticos", (req, res) => {
+  res.render("templateLibro1", { layout: "planB" });
 });
 
-app.get('/new', (req, res) => {
-    res.render('addLibro', {layout:'index'});
+app.get("/todoonada", (req, res) => {
+  res.render("templateLibro2", { layout: "planB" });
+});
+
+app.get("/new", (req, res) => {
+  res.render("addLibroTemplate", { layout: "index" });
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
-
-
-
-
-
